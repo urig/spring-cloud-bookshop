943b0e8244d80d01d9b4f74d3b8685876e97b792
Uri Goldstein <uri.goldstein@gmail.com>
2018-03-13 19:18:02 +0200

    Adding Eurkea Client to inventory billing and checkout

--- inventory/src/main/java/com/example/inventory/InventoryApplication.java
+++ inventory/src/main/java/com/example/inventory/InventoryApplication.java
@@ -5,9 +5,11 @@ import java.math.BigDecimal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryApplication {
 
 	public static void main(String[] args) {
