9088d3b88a600d38fddcdcdb8c959a665bd704ee
Uri Goldstein <uri.goldstein@gmail.com>
2018-03-13 15:01:36 +0200

    BillingClient using Feign

---
+++ checkout/src/main/java/com/example/checkout/BillingClient.java
@@ -0,0 +1,14 @@
+package com.example.checkout;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="billing", url="http://localhost:8100/billing")
+public interface BillingClient {
+
+	@PutMapping(value = "creditCardCharge")
+	public boolean chargeCreditCard(@RequestBody CreditCard creditCard);
+
+}
