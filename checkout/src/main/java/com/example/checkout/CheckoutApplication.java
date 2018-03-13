fbff5fc6b9d676c867fcbcbd3d2cc934b42e5faa
Uri Goldstein <uri.goldstein@gmail.com>
2018-03-13 15:02:08 +0200

    Enabling Feign in checkout service

--- checkout/src/main/java/com/example/checkout/CheckoutApplication.java
+++ checkout/src/main/java/com/example/checkout/CheckoutApplication.java
@@ -2,8 +2,10 @@ package com.example.checkout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CheckoutApplication {
 
 	public static void main(String[] args) {
