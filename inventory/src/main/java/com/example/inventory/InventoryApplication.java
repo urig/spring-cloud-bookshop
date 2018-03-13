0f51aa316ae713ae0f7923d53d27b52e1572c436
Uri Goldstein <uri.goldstein@gmail.com>
2018-03-13 12:11:23 +0200

    Initializing DB in CommandLineRunner

--- inventory/src/main/java/com/example/inventory/InventoryApplication.java
+++ inventory/src/main/java/com/example/inventory/InventoryApplication.java
@@ -1,12 +1,21 @@
 package com.example.inventory;
 
+import java.math.BigDecimal;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner getInit(BookRepository bookRepository) {
+		return (args) -> bookRepository.save(new Book(1, "Moby Dick", "Herman Melville", new BigDecimal(30), 10));
 	}
 }
